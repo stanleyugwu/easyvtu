5a052fe7501bef41b17c31dcb7c5f07b362a2fc7
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-09-30 02:21:23 +0100

    feat: install and setup tailwind package

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0F035B',
+        secondary: '#F2994A',
+        blue: '#1D06AD',
+        dark: '#020202',
+        gray: '#676767',
+        black: '#333333',
+        white: '#FFFFFF',
+        gray5:"#E0E0E0",
+        gray4:"#BDBDBD",
+      },
+      fontFamily: {
+        muli: ['muli', 'sans-serif'],
+      },
+      fontSize: {
+        title: '28',
+        subTitle: '24',
+        paragraph: '18',
+        caption:'16'
+      },
+    },
+  },
+  plugins: [
+  ],
+};
