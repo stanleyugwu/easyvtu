4546c027992f6e1955e40e3e04a0065bd92b083c
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-10-01 00:19:40 +0100

    feat: create custom Button component and install linear-gradient package

---
+++ components/Button.tsx
@@ -0,0 +1,62 @@
+//import libraries
+import React from 'react';
+import {TouchableOpacity, TouchableOpacityProps} from 'react-native';
+import Text from './Text';
+import tw from '../lib/tailwind';
+import LinearGradient from 'react-native-linear-gradient';
+
+export interface ButtonProps extends TouchableOpacityProps {
+  /** Button label text */
+  label: string;
+  /**
+   * The type of gradient to apply to the button.
+   * Either primary(dark-blue to blue), or secondary (orange to light orange)
+   */
+  gradientType?: 'primary' | 'secondary';
+  /**
+   * Custom array of colors to use for button gradient
+   */
+  gradient?: (string | number)[];
+}
+
+// Button Component
+const Button = ({
+  gradientType = 'primary',
+  label,
+  children,
+  style,
+  gradient,
+  ...otherProps
+}: ButtonProps) => {
+  const primaryGradient = [
+    tw.color('primary'),
+    tw.color('blue'),
+    tw.color('blue'),
+  ] as string[];
+  const secondaryGradient = [tw.color('secondary'), '#FBD40A'] as string[];
+
+  return (
+    <TouchableOpacity
+      activeOpacity={0.9}
+      style={[tw`rounded-md`, style]}
+      {...otherProps}>
+      <LinearGradient
+        style={tw`p-3.5 md:p-4 lg:p-5 rounded-md`}
+        start={{x: 0, y: 0}}
+        end={{x: 1, y: 0}}
+        colors={
+          gradient
+            ? gradient
+            : gradientType === 'primary'
+            ? primaryGradient
+            : secondaryGradient
+        }>
+        <Text color="white" style={tw`text-center`}>
+          {label}
+        </Text>
+      </LinearGradient>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
