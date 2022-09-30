0c081a984b331c9ec94119a1afeacf340d28c559
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-09-30 23:39:04 +0100

    improvement: extract colors and fonts theme to exportable variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,31 @@
 
-/**
- * @type {import("twrnc").TwConfig}
- */
+const TwColors = {
+  primary: '#0F035B',
+  secondary: '#F2994A',
+  blue: '#1D06AD',
+  dark: '#020202',
+  gray: '#676767',
+  black: '#333333',
+  white: '#FFFFFF',
+  gray5:"#E0E0E0",
+  gray4:"#BDBDBD",
+};
+
+const TwFontSize = {
+  title: '28',
+  subTitle: '24',
+  paragraph: '18',
+  caption:'16'
+}
+
 const config = {
   theme: {
     extend: {
-      colors: {
-        primary: '#0F035B',
-        secondary: '#F2994A',
-        blue: '#1D06AD',
-        dark: '#020202',
-        gray: '#676767',
-        black: '#333333',
-        white: '#FFFFFF',
-        gray5:"#E0E0E0",
-        gray4:"#BDBDBD",
-      },
+      colors:TwColors,
       fontFamily: {
         muli: ['Muli', 'sans-serif'],
       },
-      fontSize: {
-        title: '28',
-        subTitle: '24',
-        paragraph: '18',
-        caption:'16'
-      },
+      fontSize:TwFontSize,
       screens:{
         sm: '380px',
       md: '420px',
