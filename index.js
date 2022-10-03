6ac4a8e3d8457cb6f2e51de6a3cb2961d7fd357f
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-10-03 01:28:24 +0100

    feat: setup secure storage package

--- index.js
+++ index.js
@@ -5,5 +5,12 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {MMKVLoader} from 'react-native-mmkv-storage';
+
+// populate storage adapter to global object
+globalThis.secureStorage = new MMKVLoader()
+  .withEncryption()
+  .withInstanceID('secure_creds')
+  .initialize()
 
 AppRegistry.registerComponent(appName, () => App);
