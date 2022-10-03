7d8529a63c0f3678d915b313f7bcb729ef92a7c5
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-10-03 14:17:46 +0100

    feat: create generic scrollview and safe area view component

---
+++ components/SafeAreaScrollView.tsx
@@ -0,0 +1,49 @@
+//import libraries
+import React from 'react';
+import {Image, ScrollView, ScrollViewProps} from 'react-native';
+import tw from '../lib/tailwind';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import tileImg from '../assets/images/tile.png';
+import {TextProps} from './Text';
+
+interface SafeAreaScrollViewProps extends ScrollViewProps {
+  /**
+   * Background color of the view conforming to app design
+   */
+  backgroundColor?: TextProps['color'];
+  /**
+   * Whether to add tile background image
+   */
+  showTile?: boolean;
+}
+
+/**
+ * Customised `SafeAreaView` component which wraps children in `ScrollView`
+ * and can also add app image background tile
+ */
+const SafeAreaScrollView = ({
+  backgroundColor = 'gray5',
+  showTile = true,
+  children,
+  ...otherProps
+}: SafeAreaScrollViewProps) => {
+  return (
+    <SafeAreaView
+      style={[
+        tw.style(`flex-1`, {backgroundColor: tw.color(backgroundColor)}),
+      ]}>
+      {showTile ? (
+        <Image
+          source={tileImg}
+          style={{position: 'absolute', right: 0}}
+          resizeMethod="resize"
+        />
+      ) : null}
+      <ScrollView style={tw`p-4`} {...otherProps}>
+        {children}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SafeAreaScrollView;
