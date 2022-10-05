efd92ed5656531506c47105778f13d2512241cb6
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-10-05 03:07:19 +0100

    feat: create re-usable full-screen animative loader component

---
+++ components/Loader.tsx
@@ -0,0 +1,39 @@
+//import libraries
+import React from 'react';
+import tw from '../lib/tailwind';
+import AnimatedLottieView, {AnimatedLottieViewProps} from 'lottie-react-native';
+import loader from '../assets/json/loader.json';
+import * as Animatable from 'react-native-animatable';
+
+interface LoaderProps extends Omit<AnimatedLottieViewProps, "source"> {
+  /**
+   * Whether loader is visible
+   */
+  visible?: boolean;
+}
+
+// Overlay loader animation component
+const Loader = ({visible = true, ...otherProps}: LoaderProps) => {
+  if (!visible) return null;
+
+  return (
+    <Animatable.View
+      animation={'zoomIn'}
+      useNativeDriver
+      duration={500}
+      style={tw.style(`absolute z-50 inset-0 justify-center items-center`, {
+        backgroundColor: tw.color('dark') + 'dd',
+      })}>
+      <AnimatedLottieView
+        autoPlay
+        renderMode="AUTOMATIC"
+        style={tw`self-center w-[300px] md:w-[350px]`}
+        speed={1.7}
+        {...otherProps}
+        source={loader}
+      />
+    </Animatable.View>
+  );
+};
+
+export default Loader;
