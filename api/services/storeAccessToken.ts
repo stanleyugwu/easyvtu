e67b9f15bafa8c992844dd2d51ea71f89d117396
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-10-10 23:18:05 +0100

    improvement: add logic to persist user token

--- api/services/storeAccessToken.ts
+++ api/services/storeAccessToken.ts
@@ -1,9 +1,10 @@
 /**
- * Stores user jwt token globally
+ * Persists jwt token on user device and stores it globally for app use
  */
 const storeAccessToken = (token: string) => {
   // @ts-ignore
   globalThis.$token$ = token;
+  globalThis.secureStorage.setString('__$auth_token$__',token);
 };
 
 export default storeAccessToken;
