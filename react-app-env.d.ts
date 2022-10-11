19d9ebefaf0ca94439309798fb0bfc871b61c517
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-10-11 20:32:54 +0100

    improvement: change typescript module declaration default export type for .svg file to react component type for rich intellisense

--- react-app-env.d.ts
+++ react-app-env.d.ts
@@ -1,48 +1,47 @@
 /// <reference types="react" />
-  
-  declare module '*.avif' {
-    const src: number;
-    export default src;
-  }
-  
-  declare module '*.bmp' {
-    const src: number;
-    export default src;
-  }
-  
-  declare module '*.gif' {
-    const src: number;
-    export default src;
-  }
-  
-  declare module '*.jpg' {
-    const src: number;
-    export default src;
-  }
-  
-  declare module '*.jpeg' {
-    const src: number;
-    export default src;
-  }
-  
-  declare module '*.png' {
-    const src: number;
-    export default src;
-  }
-  
-  declare module '*.webp' {
-      const src: number;
-      export default src;
-  }
-  
-  declare module '*.svg' {
-    import * as React from 'react';
-  
-    export const ReactComponent: React.FunctionComponent<React.SVGProps<
-      SVGSVGElement
-    > & { title?: string }>;
-  
-    const src: string;
-    export default src;
-  }
-  
+
+declare module '*.avif' {
+  const src: number;
+  export default src;
+}
+
+declare module '*.bmp' {
+  const src: number;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: number;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: number;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: number;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: number;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: number;
+  export default src;
+}
+
+declare module '*.svg' {
+  import * as React from 'react';
+
+  const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & {title?: string}
+  >;
+
+  export const src: string;
+  export default ReactComponent;
+}
