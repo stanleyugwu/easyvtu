2e48b351c858b4e76819322d819ab5a32c916291
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-10-11 20:42:52 +0100

    feat: create metro custom resolver for `assets:` and `components:` import aliases

--- metro.config.js
+++ metro.config.js
@@ -4,24 +4,51 @@
  *
  * @format
  */
- const { getDefaultConfig } = require("metro-config");
+const {getDefaultConfig} = require('metro-config');
+const path = require('path');
 
- module.exports = (async () => {
-   const {
-     resolver: { sourceExts, assetExts },
-   } = await getDefaultConfig();
-   return {
-     transformer: {
-       babelTransformerPath: require.resolve(
-         "react-native-svg-transformer"
-       ),
-     },
-     resolver: {
-       assetExts: assetExts.filter((ext) => ext !== "svg"),
-       sourceExts: [...sourceExts, "svg"],
-     },
-   };
- })();
+module.exports = (async () => {
+  const {
+    resolver: {sourceExts, assetExts},
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+      // below resolver will resolve imports starting with
+      // `assets:` and `components:` to app assets and components
+      // folder respetively
+      resolveRequest: (context, moduleName, platform) => {
+        // resolves `assets:`
+        if (moduleName.startsWith('assets:')) {
+          const realPath = moduleName.substring(moduleName.indexOf(':') + 1);
+          const resolvedPath = path.resolve('assets', realPath);
+          return {
+            // NOTE: below is supposed to be `filePaths` and `type:'assetFiles'`
+            // but it throws error from metro Module resolver
+            filePath: resolvedPath,
+            type: 'sourceFile',
+          };
+        }
+
+        // // resolves `components:`
+        if (moduleName.startsWith('components:')) {
+          const realPath = moduleName.substring(moduleName.indexOf(':') + 1);
+          const resolvedPath = path.resolve('components', realPath);
+          return {
+            filePath: resolvedPath,
+            type: 'sourceFile',
+          };
+        }
+        // import doesn't start with `assets/`
+        return context.resolveRequest(context, moduleName, platform);
+      },
+    },
+  };
+})();
 
 // module.exports = {
 //   transformer: {
