767ba76a4f07645d01a0d5a819615127a3183052
Stanley Ugwu <stanleyugwu2018@gmail.com>
2022-10-12 16:58:56 +0100

    feat: create transaction button component for rendering pressable wallet tx history

---
+++ screens/wallet/TransactionCard.tsx
@@ -0,0 +1,72 @@
+//import libraries
+import React from 'react';
+import {View, TouchableOpacity, TouchableOpacityProps} from 'react-native';
+import Text from '../../components/Text';
+import tw from '../../lib/tailwind';
+import DepositIcon from 'assets:images/deposit.svg';
+import WithdrawIcon from 'assets:images/withdraw.svg';
+import {format} from 'timeago.js';
+import formatAmount from '../../utils/formatAmount';
+
+interface TransactionCardProps extends TouchableOpacityProps {
+  /**
+   * transaction type
+   */
+  transactionType: 'deposit' | 'withdrawal';
+
+  /**
+   * widthdrawal or deposit transaction method
+   */
+  method: string;
+
+  /**
+   * transaction date
+   */
+  date: string | number;
+
+  /**
+   * transaction amount
+   */
+  amount: number;
+}
+
+// TransactionCard Component
+const TransactionCard = ({
+  amount,
+  date = Date.now(),
+  method,
+  transactionType = 'deposit',
+  style,
+  ...otherProps
+}: TransactionCardProps) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.9}
+      style={[
+        tw`bg-white rounded-md shadow-sm flex-row items-center justify-between px-4 py-2 my-2`,
+        style,
+      ]}
+      {...otherProps}>
+      <View style={tw`flex-row items-center justify-center`}>
+        {transactionType === 'deposit' ? <DepositIcon /> : <WithdrawIcon />}
+        <View style={tw`ml-3`}>
+          <Text color="gray" type="caption">
+            {method}
+          </Text>
+          <Text color="black">
+            {transactionType === 'deposit' ? 'Deposit' : 'withdrawal'}
+          </Text>
+          <Text type="caption" color="gray">
+            {format(new Date(date))}
+          </Text>
+        </View>
+      </View>
+      <Text color="black">
+        {'\u20A6'}
+        {formatAmount(amount)}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default TransactionCard;
